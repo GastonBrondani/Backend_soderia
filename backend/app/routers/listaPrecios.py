77b9f29becf532db6214161b2f22bfcd40a7608c
Emmanuel Quintana <emmanuelquintanafattor@gmail.com>
2026-04-03 21:23:58 -0300

    resolver error del merge

--- backend/app/routers/listaPrecios.py
+++ backend/app/routers/listaPrecios.py
@@ -97,26 +97,10 @@ def listar_listas(db: Session = Depends(get_db), limit: int = 50, offset: int = 
     return svc_listar_listas(db, limit, offset)
 
 
-# # Devuelve todos los productos que tienen precio cargado en la lista indicada, junto con ese precio.
-# @router.get(
-#     "/{id_lista}/productos", response_model=List[ProductoConPrecioOut]
-# )  # Usado por emma.
-# @router.get("/{id_lista}", response_model=ListaDePreciosOut)
-# def obtener_lista(id_lista: int, db: Session = Depends(get_db)):
-#     return svc_obtener_lista(db, id_lista)
-
-# @router.get("/", response_model=list[ListaDePreciosOut])  # Usado por emma.
-# def listar_listas(db: Session = Depends(get_db), limit: int = 50, offset: int = 0):
-#     return svc_listar_listas(db, limit, offset)
-
-
-# # Devuelve todos los productos que tienen precio cargado en la lista indicada, junto con ese precio.
-# @router.get(
-#     "/{id_lista}/productos", response_model=List[ProductoConPrecioOut]
-# )  # Usado por emma.
-# @router.get("/{id_lista}", response_model=ListaDePreciosOut)
-# def obtener_lista(id_lista: int, db: Session = Depends(get_db)):
-#     return svc_obtener_lista(db, id_lista)
+# Devuelve todos los productos que tienen precio cargado en la lista indicada, junto con ese precio.
+@router.get("/{id_lista}", response_model=ListaDePreciosOut)
+def obtener_lista(id_lista: int, db: Session = Depends(get_db)):
+    return svc_obtener_lista(db, id_lista)
 
 
 @router.put("/{id_lista}", response_model=ListaDePreciosOut)
@@ -144,6 +128,7 @@ def listar_productos_con_precio(
     en la lista indicada, junto con ese precio.
     """
     return svc_listar_productos_con_precio_por_lista(db, id_lista)
+
 
 
 """ @router.get("/{id_lista}/precios-combos", response_model=List[LPCBasicOutCombo])
